66986d8ac512e7543e0964270ae86a4f0629659c
opilane <opilane@HP-CZC2349HTQ>
2024-10-08 13:59:29 +0300

    Add project files.

---
+++ Controllers/PrimitiividController.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace veeb.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PrimitiividController : ControllerBase
+    {
+        // GET: primitiivid/hello-world
+        [HttpGet("hello-world")]
+        public string HelloWorld()
+        {
+            return "Hello world at " + DateTime.Now;
+        }
+
+        // GET: primitiivid/hello-variable/mari
+        [HttpGet("hello-variable/{nimi}")]
+        public string HelloVariable(string nimi)
+        {
+            return "Hello " + nimi;
+        }
+
+        // GET: primitiivid/add/5/6
+        [HttpGet("add/{nr1}/{nr2}")]
+        public int AddNumbers(int nr1, int nr2)
+        {
+            return nr1 + nr2;
+        }
+
+        // GET: primitiivid/multiply/5/6
+        [HttpGet("multiply/{nr1}/{nr2}")]
+        public int Multiply(int nr1, int nr2)
+        {
+            return nr1 * nr2;
+        }
+
+        // GET: primitiivid/do-logs/5
+        [HttpGet("do-logs/{arv}")]
+        public void DoLogs(int arv)
+        {
+            for (int i = 0; i < arv; i++)
+            {
+                Console.WriteLine("See on logi nr " + i);
+            }
+        }
+
+        // GET: primitiivid/random/5/15
+        [HttpGet("random/{min}/{max}")]
+        public int GetRandomNumber(int min, int max)
+        {
+            Random rand = new Random();
+            return rand.Next(min, max + 1); // +1, et max kaasa arvatud oleks
+        }
+
+        // GET: primitiivid/age/1990
+        [HttpGet("age/{yearOfBirth}")]
+        public string GetAge(int yearOfBirth)
+        {
+            var currentYear = DateTime.Now.Year;
+            var currentMonthDay = DateTime.Now.Month * 100 + DateTime.Now.Day;
+            var birthMonthDay = new DateTime(yearOfBirth, 1, 1).Month * 100 + 1; // eeldades, et sünnipäev on 1. jaanuaril
+            int age = currentYear - yearOfBirth;
+
+            if (currentMonthDay < birthMonthDay)
+            {
+                age--; // kui sünnipäev pole veel olnud, lahutame 1
+            }
+
+            return $"Oled {age} aastat vana.";
+        }
+    }
+}
---
+++ Controllers/ToodeController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using veeb.Models;
+
+namespace veeb.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ToodeController : ControllerBase
+    {
+        private static Toode _toode = new Toode(1, "Koola", 1.5, true);
+
+        // GET: toode
+        [HttpGet]
+        public Toode GetToode()
+        {
+            return _toode;
+        }
+
+        // GET: toode/suurenda-hinda
+        [HttpGet("suurenda-hinda")]
+        public Toode SuurendaHinda()
+        {
+            _toode.Price = _toode.Price + 1;
+            return _toode;
+        }
+
+        // GET: toode/muuda-aktiivsust
+        [HttpGet("muuda-aktiivsust")]
+        public Toode MuudaAktiivsust()
+        {
+            _toode.IsActive = !_toode.IsActive; // Muudab aktiivsust vastupidiseks
+            return _toode;
+        }
+
+        // GET: toode/muuda-nime/UusNimi
+        [HttpGet("muuda-nime/{uusNimi}")]
+        public Toode MuudaNime(string uusNimi)
+        {
+            _toode.Name = uusNimi; // Määrab uue nime URL-i kaudu
+            return _toode;
+        }
+
+        // GET: toode/muuda-hinda/2
+        [HttpGet("muuda-hinda/{kordaja}")]
+        public Toode MuudaHinda(double kordaja)
+        {
+            _toode.Price = _toode.Price * kordaja; // Korrutab hinna antud kordajaga
+            return _toode;
+        }
+    }
+}
