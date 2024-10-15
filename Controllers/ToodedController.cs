a442d15b6003e3ab7a5f7e552de042b72422326b
opilane <opilane@HP-CZC2349HV1>
2024-10-15 14:49:48 +0300

    hakassin teha node.js

--- Controllers/ToodedController.cs
+++ Controllers/ToodedController.cs
@@ -8,37 +8,46 @@ namespace veeb.Controllers
     [ApiController]
     public class ToodedController : ControllerBase
     {
-        private static List<Toode> _tooted = new List<Toode>{
-        new Toode(1,"Koola", 1.5, true),
-        new Toode(2,"Fanta", 1.0, false),
-        new Toode(3,"Sprite", 1.7, true),
-        new Toode(4,"Vichy", 2.0, true),
-        new Toode(5,"Vitamin well", 2.5, true)
+        private static List<Toode> _tooted = new()
+        {
+            new Toode(1,"Koola", 1.5, true),
+            new Toode(2,"Fanta", 1.0, false),
+            new Toode(3,"Sprite", 1.7, true),
+            new Toode(4,"Vichy", 2.0, true),
+            new Toode(5,"Vitamin well", 2.5, true)
         };
 
-
-        // https://localhost:7052/tooted
+        // GET https://localhost:4444/tooted
         [HttpGet]
         public List<Toode> Get()
         {
             return _tooted;
         }
 
-        [HttpGet("kustuta/{index}")]
+        // DELETE https://localhost:4444/tooted/kustuta/0
+        [HttpDelete("kustuta/{index}")]
         public List<Toode> Delete(int index)
         {
             _tooted.RemoveAt(index);
             return _tooted;
         }
 
-        [HttpGet("kustuta2/{index}")]
+        [HttpDelete("kustuta2/{index}")]
         public string Delete2(int index)
         {
             _tooted.RemoveAt(index);
             return "Kustutatud!";
         }
 
-        [HttpGet("lisa/{id}/{nimi}/{hind}/{aktiivne}")]
+        [HttpPost("lisa")]
+        public List<Toode> Add([FromBody] Toode toode)
+        {
+            _tooted.Add(toode);
+            return _tooted;
+        }
+
+        // POST https://localhost:4444/tooted/lisa/1/Coca/1.5/true
+        [HttpPost("lisa/{id}/{nimi}/{hind}/{aktiivne}")]
         public List<Toode> Add(int id, string nimi, double hind, bool aktiivne)
         {
             Toode toode = new Toode(id, nimi, hind, aktiivne);
@@ -46,16 +55,17 @@ namespace veeb.Controllers
             return _tooted;
         }
 
-        [HttpGet("lisa")] // GET /tooted/lisa?id=1&nimi=Koola&hind=1.5&aktiivne=true
-        public List<Toode> Add2([FromQuery] int id, [FromQuery] string nimi, [FromQuery] double hind, [FromQuery] bool aktiivne)
+        [HttpPost("lisa2")]
+        public List<Toode> Add2(int id, string nimi, double hind, bool aktiivne)
         {
             Toode toode = new Toode(id, nimi, hind, aktiivne);
             _tooted.Add(toode);
             return _tooted;
         }
 
-        [HttpGet("hind-dollaritesse/{kurss}")] // GET /tooted/hind-dollaritesse/1.5
-        public List<Toode> Dollaritesse(double kurss)
+        // PATCH https://localhost:4444/tooted/hind-dollaritesse/1.5
+        [HttpPatch("hind-dollaritesse/{kurss}")]
+        public List<Toode> UpdatePrices(double kurss)
         {
             for (int i = 0; i < _tooted.Count; i++)
             {
--- Program.cs
+++ Program.cs
@@ -18,6 +18,12 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseCors(options => options
+    .WithOrigins("http://localhost:3000")
+    .AllowAnyMethod()
+    .AllowAnyHeader()
+);
+
 app.UseAuthorization();
 
 app.MapControllers();
